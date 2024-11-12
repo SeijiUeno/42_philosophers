5425c9eaa16182fc1fb982466d99aec28e3729d6
-Seiji <142241481+SeijiUeno@users.noreply.github.com>
2024-11-12 18:24:16 -0300

    Revert "Philosopher entity"

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: sueno-te <sueno-te@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 14:37:29 by sueno-te          #+#    #+#             */
-/*   Updated: 2024/11/12 17:26:11 by sueno-te         ###   ########.fr       */
+/*   Updated: 2024/11/12 16:10:48 by sueno-te         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "philosopher.h"
 
-static int loading(t_table *holder, int argc, char *argv[])
+static int loading(t_init *holder, int argc, char *argv[])
 {
     if (!basic_check(argc, argv))
     {
@@ -25,7 +25,7 @@ static int loading(t_table *holder, int argc, char *argv[])
 
 int main(int argc, char *argv[])
 {
-    t_table  holder;
+    t_init  holder;
 
     loading(&holder, argc, argv);
     return (0);
